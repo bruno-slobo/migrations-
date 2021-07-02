903f5aef23ccfb51859811aabdd09bb105394bc6
bruno-slobo <64993455+bruno-slobo@users.noreply.github.com>
2021-07-02 06:51:29 -0300

    Add files via upload

---
+++ database/migrations/2021_07_01_024406_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // criando a tabela de produtos 
+        Schema::create('products', function (Blueprint $table) {
+            // inserindo atributos na tabela
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
